f4bd4154bee0da7a269a4156fb9e925e3682afe9
amen-meddle-east <47331868+amen-meddle-east@users.noreply.github.com>
2019-02-16 14:01:28 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,81 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = "/"
+const devs = ["406076958980571147"];
+const adminprefix = ["/bc"];
+
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`امن الشرق الاوسط`,"")
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+});
+
+
+client.on("message", message => {
+        if (message.author.id === client.user.id) return;
+        if (message.guild) {
+       let embed = new Discord.RichEmbed()
+        let args = message.content.split(' ').slice(1).join(' ');
+    if(message.content.split(' ')[0] == prefix + 'bc') {
+        if (!args[1]) {
+    message.channel.send(prefix +"bc");
+    return;
+    }
+            message.guild.members.forEach(m => {
+       if(!message.member.hasPermission('ADMINISTRATOR')) return;
+                m.send(args);
+            });
+            const AziRo = new Discord.RichEmbed()
+            .setAuthor(message.author.username, message.author.avatarURL)   
+            .setTitle('✅| جاري ارسال رسالتك ') 
+            .addBlankField(true)
+            .addField('♨| عدد المستلمين ', message.guild.memberCount , true)        
+            .addField('📝| الرسالة ', args)
+            .setColor('RANDOM')  
+            message.channel.sendEmbed(AziRo);          
+        }
+        } else {
+            return;
+        }
+    });
+
+
+
+
+client.on('message', message => {
+    if (message.content === adminprefix + "shelp") {
+        let helpEmbed = new Discord.RichEmbed()
+        .setTitle('**...اوامر الستريم...**')
+        .addField('name','لتغيير الاسم')
+        .addField('avatar','لتغير الصورة')
+        .addField('twitch','لوضع تويتش')
+        .addField('play', 'لوضع بلاينق')
+        .addField('watch','لوضع واتشينق')
+	.addField('listen','ليسين')
+        .setFooter('(,help) لاظهار الاوامر ')
+      message.channel.send(helpEmbed);
+    }
+});
+
+client.login(process.env.BOT_TOKEN);
